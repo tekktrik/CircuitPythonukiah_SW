a6bd4f31faf509f46229fe6f90cd9f9ba867ee10
Alec Delaney <tekktrik@gmail.com>
2022-08-02 17:12:13 -0400

    Add REUSE header and module docstring

--- settings.py
+++ settings.py
@@ -1,1 +1,15 @@
+# SPDX-FileCopyrightText: 2022 Alec Delaney
+#
+# SPDX-License-Identifier: MIT
+
+"""
+`settings.py`
+=============
+
+Optional settings for your menorah!
+
+* Author: Alec Delaney
+
+"""
+
 BURNOUT = True
