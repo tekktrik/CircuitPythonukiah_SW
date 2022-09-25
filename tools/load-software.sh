9feb2aac02e49cf13cbb16ea2725efda385cb108
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-09-24 23:11:20 -0400

    Fully automate loading software

--- tools/load-software.sh
+++ tools/load-software.sh
@@ -2,12 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 
+# Get username and device path
+username="$(whoami)"
+devicepath="/run/media/$username/CIRCUITPY"
+
 # Copy the code from microcontroller/ over to the ESP32-S2
-cp -r microcontroller/. $1
+cp -r microcontroller/. $devicepath
 
 # Install libraries via circup
 circup install -r requirements-circup.txt
 
 # Create secrets.py for the device
-cp tools/secrets_template.py $1/
-mv $1/secrets_template.py $1/secrets.py
+cp tools/secrets_template.py $devicepath
+mv $devicepath/secrets_template.py $devicepath/secrets.py
